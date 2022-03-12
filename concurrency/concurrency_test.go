8192fd0525ccca46e34ea1afcecabb32abd56157
Siddhanth <bnsiddhanth0@gmail.com>
2022-03-12 14:08:07 +0530

    L11: COncurrency

---
+++ concurrency/concurrency_test.go
@@ -0,0 +1,52 @@
+package concurrency
+
+import (
+	"reflect"
+	"testing"
+	"time"
+)
+
+func mockWebsiteChecker(url string) bool {
+	if url == "waat://furhurterwe.geds" {
+		return false
+	}
+	return true
+}
+
+func slowStubWebsiteChecker(url string) bool {
+	time.Sleep(20 * time.Millisecond)
+	return true
+}
+
+func TestCheckWebsites(t *testing.T) {
+	websites := []string{
+		"http://google.com",
+		"http://blog.gypsydave5.com",
+		"waat://furhurterwe.geds",
+	}
+
+	want := map[string]bool{
+		"http://google.com":          true,
+		"http://blog.gypsydave5.com": true,
+		"waat://furhurterwe.geds":    false,
+	}
+
+	got := checkWebsites(mockWebsiteChecker, websites)
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Wanted %v, Got: %v", want, got)
+	}
+}
+
+func BenchmarkCheckWebsites(b *testing.B) {
+	urls := make([]string, 100)
+
+	for i := 0; i < len(urls); i++ {
+		urls[i] = "a url"
+	}
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		checkWebsites(slowStubWebsiteChecker, urls)
+	}
+}
