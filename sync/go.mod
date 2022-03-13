30751683d80140a9540c5a875ad49ed54a7b1c39
Siddhanth <bnsiddhanth0@gmail.com>
2022-03-13 18:57:10 +0530

    L14: sync

---
+++ sync/go.mod
@@ -0,0 +1,3 @@
+module synco
+
+go 1.17
---
+++ sync/sync_test.go
@@ -0,0 +1,42 @@
+package synco
+
+import (
+	"sync"
+	"testing"
+)
+
+func TestSync(t *testing.T) {
+	t.Run("incrementing at 3 leaves it at 3", func(t *testing.T) {
+		counter := Counter{}
+		counter.Inc()
+		counter.Inc()
+		counter.Inc()
+
+		assertCounter(t, &counter, 3)
+	})
+
+	t.Run("works safely concurrently", func(t *testing.T) {
+		wantedCount := 1000
+		counter := Counter{}
+		var wg sync.WaitGroup
+
+		wg.Add(wantedCount)
+
+		for i := 0; i < wantedCount; i++ {
+			go func() {
+				counter.Inc()
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+
+		assertCounter(t, &counter, wantedCount)
+	})
+}
+
+func assertCounter(t testing.TB, got *Counter, want int) {
+	t.Helper()
+	if got.Value() != want {
+		t.Errorf("got %d, want %d", got.Value(), want)
+	}
+}
